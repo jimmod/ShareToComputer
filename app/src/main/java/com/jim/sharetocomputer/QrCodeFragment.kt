702c2ec41e7db84883a5b4de573601c6e1a6ba75
Jimmy <jim.halim@gmail.com>
2019-06-14 16:50:40 -0400

    - Handle unknown QR code

--- app/src/main/java/com/jim/sharetocomputer/QrCodeFragment.kt
+++ app/src/main/java/com/jim/sharetocomputer/QrCodeFragment.kt
@@ -25,6 +25,7 @@ import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.google.gson.Gson
+import com.google.gson.JsonSyntaxException
 import com.google.zxing.integration.android.IntentIntegrator
 import com.google.zxing.integration.android.IntentResult
 import com.jim.sharetocomputer.coroutines.TestableDispatchers
@@ -34,7 +35,7 @@ import kotlinx.coroutines.launch
 import timber.log.Timber
 
 
-class QrCodeFragment: Fragment() {
+class QrCodeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         GlobalScope.launch(TestableDispatchers.Default) {
@@ -44,19 +45,24 @@ class QrCodeFragment: Fragment() {
                     result.resultCode,
                     result.resultData
                 )
-                val qrCodeInfo = Gson().fromJson(resultBarcode!!.contents, QrCodeInfo::class.java)
-                Timber.d("Downloading from: $qrCodeInfo")
-                ContextCompat.startForegroundService(
-                    activity!!,
-                    DownloadService.createIntent(activity!!, qrCodeInfo.url)
-                )
+                var message = R.string.info_download_start
+                try {
+                    val qrCodeInfo = Gson().fromJson(resultBarcode!!.contents, QrCodeInfo::class.java)
+                    Timber.d("Downloading from: $qrCodeInfo")
+                    ContextCompat.startForegroundService(
+                        activity!!,
+                        DownloadService.createIntent(activity!!, qrCodeInfo.url)
+                    )
+                    if (qrCodeInfo.version > Application.QR_CODE_VERSION) {
+                        message = R.string.warning_newer_qrcode
+                    }
+                } catch (e: JsonSyntaxException) {
+                    Timber.w(e)
+                    message = R.string.warning_unknown_qrcode
+                }
 
                 GlobalScope.launch(TestableDispatchers.Main) {
-                    if (qrCodeInfo.version > Application.QR_CODE_VERSION) {
-                        Toast.makeText(activity, R.string.warning_newer_qrcode, Toast.LENGTH_LONG).show()
-                    } else {
-                        Toast.makeText(activity, R.string.info_download_start, Toast.LENGTH_LONG).show()
-                    }
+                    Toast.makeText(activity, message, Toast.LENGTH_LONG).show()
                     findNavController().popBackStack()
                 }
 
